9718e7a1c12208fee4e54bdf92c5d7df3e2cee24
yonko-mo <alsantel.ma@gmail.com>
2026-01-27 22:07:39 +0200

    correcting question_18 in session_3_homework

--- session_3_homework/lib/question_18.dart
+++ session_3_homework/lib/question_18.dart
@@ -5,21 +5,11 @@ a default. Print values in uppercase, and display 'Prod ready' or 'Non-prod' dep
 conditions.
 */
 void main() {
-  Map<String, String?> environmentVariables = {
-    'mode': 'prod',
-    'api': null,
-    'region': 'eu',
-  };
+  Map<String, String?> environmentVariables = {'debugMode': null};
 
-  if (environmentVariables['api'] == null) {
-    environmentVariables['api'] = 'NOT_SET';
-  }
-  String mode = environmentVariables['mode']!;
-  String api = environmentVariables['api']!;
-  print(environmentVariables.values.toString().toUpperCase());
-  if (mode.toUpperCase() == 'PROD' && api.toUpperCase() == "NOT_SET") {
-    print('Non-prod');
-  } else {
+  if (environmentVariables['debugMode']! == 'true') {
     print('Prod ready');
+  } else {
+    print('Non-prod');
   }
 }

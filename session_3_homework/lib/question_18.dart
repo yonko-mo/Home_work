0601430785136ed39e462e54bf225bdefc7fd5ba
yonko-mo <alsantel.ma@gmail.com>
2026-01-27 22:20:58 +0200

    correcting again question 18 in session 3_homework

--- session_3_homework/lib/question_18.dart
+++ session_3_homework/lib/question_18.dart
@@ -4,10 +4,23 @@ Write a Dart program that reads environment variables from a map. If a value is 
 a default. Print values in uppercase, and display 'Prod ready' or 'Non-prod' depending on
 conditions.
 */
+/*
+Question 18
+Write a Dart program that reads environment variables from a map. If a value is null, replace it with
+a default. Print values in uppercase, and display 'Prod ready' or 'Non-prod' depending on
+conditions.
+*/
 void main() {
   Map<String, String?> environmentVariables = {'debugMode': null};
 
-  if (environmentVariables['debugMode']! == 'true') {
+  String debugValue = environmentVariables['debugMode'] ?? 'false';
+
+  String upperValue = debugValue.toUpperCase();
+
+  print('DEBUG MODE: $upperValue');
+
+  // الشرط
+  if (upperValue == 'TRUE') {
     print('Prod ready');
   } else {
     print('Non-prod');
